bf69c4bfe6e1acff8b51eeda1ffa6f9d611bb3b0
laksheenmendis <cresclux@yahoo.com>
2020-05-24 17:16:45 -0600

    Modifying request mapping at VetController

--- pet-clinic-web/src/main/java/cresclux/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/cresclux/springframework/sfgpetclinic/controllers/VetController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
     public String listVets()
     {
         // below string corresponds to the path to the thymeleaf template (i.e. index.html)
