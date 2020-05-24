8729760277b1a63332ec9a7fea408ccf6bd0e39e
laksheenmendis <cresclux@yahoo.com>
2020-05-24 16:53:04 -0600

    Add index page and controller. Closes #8

---
+++ pet-clinic-web/src/main/java/cresclux/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package cresclux.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by laksheenmendis on 5/24/20 at 4:42 PM
+ */
+@Controller
+public class IndexController {
+
+    // below is a mapping for multiple requests
+    // Eg; localhost:9090, localhost:9090/, localhost:9090/index, localhost:9090/index.html
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index()
+    {
+        return "index";
+    }
+}
