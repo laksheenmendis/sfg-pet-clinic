fbe261f2a151bc7fcdfc73586b3b27b680102094
laksheenmendis <cresclux@yahoo.com>
2020-06-28 22:36:30 -0600

    Updated Vet JPA Entity mappings. Closes #37

--- pet-clinic-data/src/main/java/cresclux/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/cresclux/springframework/sfgpetclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package cresclux.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by laksheenmendis on 6/19/20 at 11:27 PM
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/cresclux/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/cresclux/springframework/sfgpetclinic/model/Vet.java
@@ -1,13 +1,19 @@
 package cresclux.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by laksheenmendis on 5/23/20 at 10:06 PM
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities;
 
     public Set<Speciality> getSpecialities() {
