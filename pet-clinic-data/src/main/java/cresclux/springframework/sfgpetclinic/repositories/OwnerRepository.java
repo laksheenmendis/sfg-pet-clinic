153d3eaf6c0f4e1ebf063164eee5e17dcbc9665c
laksheenmendis <cresclux@yahoo.com>
2020-06-29 00:10:39 -0600

    Added spring data jpa service for owners. Closes #40

--- pet-clinic-data/src/main/java/cresclux/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/cresclux/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
  * Created by laksheenmendis on 6/28/20 at 10:46 PM
  */
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
