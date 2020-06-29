67ebc613f5085f3e8e770dcd868554bf4382e3d4
laksheenmendis <cresclux@yahoo.com>
2020-06-29 00:59:23 -0600

    Added spring data jpa service for Visits. Closes #47

---
+++ pet-clinic-data/src/main/java/cresclux/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,51 @@
+package cresclux.springframework.sfgpetclinic.services.springdatajpa;
+
+import cresclux.springframework.sfgpetclinic.model.Visit;
+import cresclux.springframework.sfgpetclinic.repositories.VisitRepository;
+import cresclux.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by laksheenmendis on 6/29/20 at 12:54 AM
+ */
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
