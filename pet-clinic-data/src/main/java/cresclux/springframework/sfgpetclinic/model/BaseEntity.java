592f8b55f3ebe4f7fe82d792559a96b45598bce2
laksheenmendis <cresclux@yahoo.com>
2020-06-28 22:13:53 -0600

    Implement Base Entity. Closes #35

--- pet-clinic-data/src/main/java/cresclux/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/cresclux/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,13 +1,20 @@
 package cresclux.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by laksheenmendis on 5/24/20 at 11:31 AM
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     // Hibernate prefers if it's Object types, because they can be null
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
